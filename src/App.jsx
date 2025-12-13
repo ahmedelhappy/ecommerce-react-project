74c0e94d37fe8abb80c13a2a40c3db4f886bbc8d
ahmedelhappy <ahmedelhappy@gmail.com>
2025-12-13 11:37:41 +0200

    create add to cart feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route} from "react-router";
+import { Routes, Route } from "react-router";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { HomePage } from "./pages/home/HomePage";
@@ -11,20 +11,24 @@ import "./App.css";
 function App() {
   const [cart, setCart] = useState([]);
 
+  const loadCart = async () => {
+    const response = await axios.get("/api/cart-items?expand=product");
+    setCart(response.data);
+  };
+
   useEffect(() => {
-    const fetchAppData = async () => {
-      const response = await axios.get("/api/cart-items?expand=product");
-      setCart(response.data);
-    };
-    fetchAppData();
+    loadCart();
   }, []);
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart} />} />
+      <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
-      <Route path="tracking/:orderId/:productId" element={<TrackingPage cart={cart} />} />
+      <Route
+        path="tracking/:orderId/:productId"
+        element={<TrackingPage cart={cart} />}
+      />
       <Route path="*" element={<NotFoundPage cart={cart} />} />
     </Routes>
   );
